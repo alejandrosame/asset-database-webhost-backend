bfafa919d7c9620b3cd4fdec8992a0e9e285cf7a
Alejandro Sánchez Medina <alejandrosanchzmedina@gmail.com>
2020-05-23 11:14:07 +0100

    Update users read function

--- public_html/api/user/read.php
+++ public_html/api/user/read.php
@@ -14,33 +14,22 @@ $user = new User($db);
 $stmt = $user->read();
 $num = $stmt->rowCount();
 
-if ($num>0) {
-    $users_arr=array();
-    $users_arr["records"]=array();
+$users_arr=array();
+$users_arr["users"]=array();
 
-    // retrieve our table contents
-    // fetch() is faster than fetchAll()
-    // http://stackoverflow.com/questions/2770630/pdofetchall-vs-pdofetch-in-a-loop
-    while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
-        extract($row);
+while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+    extract($row);
 
-        $user_item=array(
-            "id" => $id,
-            "username" => $username,
-            "email" => $email,
-            "iasadmin" => $isadmin,
-            "created" => $created,
-            "updated" => $updated
-        );
-
-        array_push($users_arr["records"], $user_item);
-    }
-
-    http_response_code(200);
-    echo json_encode($users_arr);
-} else {
-    http_response_code(404);
-    echo json_encode(
-        array("message" => "No users found.")
+    $user_item=array(
+        "id" => $id,
+        "username" => $username,
+        "admin" => (boolean)json_decode(strtolower($isadmin)),
+        "created" => $created,
+        "updated" => $updated
     );
+
+    array_push($users_arr["users"], $user_item);
 }
+
+http_response_code(200);
+echo json_encode($users_arr);
